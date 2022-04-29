d5aa738100065f0c203a4fb191269c8ea60ed3b5
Jonathan Missler <jonathanmissler@MacBook-Pro-4.fritz.box>
2022-04-29 15:00:45 +0200

    built gaussian family

--- NAMLSS/families.py
+++ NAMLSS/families.py
@@ -1,11 +1,18 @@
 import tensorflow_probability as tfp
+import tensorflow as tf
 
 
-class Gaussian():
+class Gaussian:
 
-    def __init__(self, estimate_scale = True):
+    def __init__(self, estimate_scale=True):
         self.est_scale = estimate_scale
 
     def loss(self, loc, scale, val):
 
-
+        if self.est_scale:
+            dist = tfp.distributions.Normal(loc=loc, scale=tf.exp(scale))
+            out = -tf.reduce_sum(dist.log_prob(value=val))
+        else:
+            mse = tf.keras.losses.MeanSquaredError()
+            out = mse(val, loc)
+        return out
