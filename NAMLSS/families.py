ad989938c6a5cffc6129e1624a493d0f48ade04f
Jonathan Missler <jonathanmissler@MacBook-Pro-4.fritz.box>
2022-04-29 16:41:23 +0200

    added log_likelihood

--- NAMLSS/families.py
+++ NAMLSS/families.py
@@ -4,15 +4,23 @@ import tensorflow as tf
 
 class Gaussian:
 
-    def __init__(self, estimate_scale=True):
-        self.est_scale = estimate_scale
+    def __init__(self, two_param=True):
+        self.two_param = two_param
 
     def loss(self, loc, scale, val):
 
-        if self.est_scale:
+        if self.two_param:
             dist = tfp.distributions.Normal(loc=loc, scale=tf.exp(scale))
             out = -tf.reduce_sum(dist.log_prob(value=val))
         else:
             mse = tf.keras.losses.MeanSquaredError()
             out = mse(val, loc)
+        return out
+
+    def log_likelihood(self, loc, scale, val):
+
+        if self.two_param:
+            dist = tfp.distributions.Normal(loc=loc, scale=tf.exp(scale))
+            out = tf.reduce_sum(dist.log_prob(value=val))
+
         return out
