726f63e57e61ce63a65d89b0117a02035984c804
Jonathan Missler <jonathanmissler@MacBook-Pro-4.fritz.box>
2022-04-29 14:29:02 +0200

    build gaussian

--- NAMLSS/families.py
+++ NAMLSS/families.py
@@ -1,1 +1,11 @@
 import tensorflow_probability as tfp
+
+
+class Gaussian():
+
+    def __init__(self, estimate_scale = True):
+        self.est_scale = estimate_scale
+
+    def loss(self, loc, scale, val):
+
+
